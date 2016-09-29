d3fdada7c1de69d30059f878e1bf620acce36277
philanc <philanc@users.noreply.github.com>
2016-09-28 20:18:21 -0400

    added mark, exch_mark, wipe

--- junk/editfile.lua
+++ junk/editfile.lua
@@ -1,5 +1,5 @@
 -- editfile.lua
-
+-- !!! WORK IN PROGRESS !!!
 ------------------------------------------------------------------------
 
 local he = require"he"
@@ -39,7 +39,7 @@ end
 
 
 ------------------------------------------------------------------------
---display functions
+--display functions  (styles, boxes, line display)
 
 local style = {
 	normal = function() color(col.normal) end, 
@@ -51,6 +51,9 @@ local style = {
 }
 
 local function boxnew(x, y, l, c)
+	-- a box is a rectangular area on the screen
+	-- defined by top left corner (x, y) 
+	-- and number of lines and columns (l, c)
 	local b = {x=x, y=y, l=l, c=c}
 	b.clrl = rep(" ", c) -- used to clear box content
 	return b
@@ -63,6 +66,18 @@ local function boxclear(b)
 	end
 end
 
+local function boxfill(b, ch, stylefn)
+	local filler = rep(ch, b.c)
+	stylefn()
+	for i = 1, b.l do
+		go(b.x+i-1, b.y); out(filler)
+	end
+	style.normal() -- back to notmal style
+	flush()
+end
+
+-- line display
+
 local tabln = 4
 local EOL = char(187) -- >>, indicate more undisplayed chars in s
 local NDC = char(183) -- middledot, used for non-displayable latin1 chars
@@ -71,7 +86,7 @@ local EOT = '~'  -- used to indicate that we are past the end of text
 
 local function ccrepr(b, j)
 	-- return display representation of char with code b
-	-- at line offset j (used for tabs)
+	-- at line offset j (j is used for tabs)
 	local s
 	if b == 9 then s = rep(' ', tabln - j % tabln)
 	elseif (b >= 127 and b <160) or (b < 32) then s = NDC
@@ -109,17 +124,6 @@ local function boxline(b, bl, l, insel, jon, joff)
 end --boxline
 
 
-local function boxfill(b, ch, stylefn)
-	local filler = rep(ch, b.c)
-	for i = 1, b.l do
-		stylefn()
-		go(b.x+i-1, b.y)
-		out(filler)
-	end
-	style.normal() -- back to notmal style
-	flush()
-end
-
 ------------------------------------------------------------------------
 -- editor is the global editor object
 local editor = {
@@ -129,6 +133,41 @@ local editor = {
 }
 
 
+------------------------------------------------------------------------
+-- dialog functions
+
+local function msg(m)
+	-- display a message m on last screen line
+	m = pad(m, editor.scrc)
+	go(editor.scrl, 1); cleareol(); style.msg()
+	out(m); style.normal(); flush()
+end
+
+local function readstr(prompt)
+	-- display prompt, read a string on the last screen line
+	-- [read only ascii or latin1 printable chars - no tab]
+	-- [ no edition except bksp ]
+	-- if ^G then return nil
+	local function disp(s) 
+		
+	end	local s = ""
+	msg(prompt)
+	while true do
+		-- display s
+		go(editor.scrl, #prompt+1); cleareol(); outf(s)
+		k = editor.nextk()
+		if (k >= 32 and k <127) or (k >=160 and k < 256) then
+			s = s .. char(k) 
+		elseif k == 8 or k == keys.del then -- backspace
+			s = s:sub(1, -2)
+		elseif k == 13 then return s  -- return
+		elseif k == 7 then return nil -- ^G - abort
+		else -- ignore all other keys
+		end
+	end--while
+end --readstr
+
+------------------------------------------------------------------------
 -- buf is the current buffer
 local buf = {}
 
@@ -206,7 +245,7 @@ local function displaylines(buf)
 	flush()
 end
 
-local function bufredisplay(buf)
+local function redisplay(buf)
 	adjcursor(buf)
 	if buf.chgd or buf.si then
 		displaylines(buf)
@@ -214,7 +253,7 @@ local function bufredisplay(buf)
 		adjcursor(buf)
 	end
 	buf.chgd = false
-end --bufredisplay
+end --redisplay
 
 local function fullredisplay()
 	editor.scrl, editor.scrc = term.getscrlc()
@@ -224,42 +263,8 @@ local function fullredisplay()
 	boxfill(editor.scrbox, NDC, style.bckg)
 	buf.box = boxnew(2, 2, editor.scrl-3, editor.scrc-2)
 	buf.chgd = true
-	bufredisplay(buf)
+	redisplay(buf)
 end --fullredisplay
-
-------------------------------------------------------------------------
--- dialog functions
-
-local function msg(m)
-	-- display a message m on last screen line
-	m = pad(m, editor.scrc)
-	go(editor.scrl, 1); cleareol(); style.msg()
-	out(m); style.normal(); flush()
-end
-
-local function readstr(prompt)
-	-- display prompt, read a string on the last screen line
-	-- [read only ascii or latin1 printable chars - no tab]
-	-- [ no edition except bksp ]
-	-- if ^G then return nil
-	local function disp(s) 
-		
-	end	local s = ""
-	msg(prompt)
-	while true do
-		-- display s
-		go(editor.scrl, #prompt+1); cleareol(); outf(s)
-		k = editor.nextk()
-		if (k >= 32 and k <127) or (k >=160 and k < 256) then
-			s = s .. char(k) 
-		elseif k == 8 or k == keys.del then -- backspace
-			s = s:sub(1, -2)
-		elseif k == 13 then return s  -- return
-		elseif k == 7 then return nil -- ^G - abort
-		else -- ignore all other keys
-		end
-	end--while
-end --readstr
 
 ------------------------------------------------------------------------
 -- buffer utility functions
@@ -274,10 +279,35 @@ local function atbol(buf) return buf.cj <= 0 end
 local function ateot(buf) return (buf.ci == #buf.ll) and ateol(buf) end
 local function atbot(buf) return (buf.ci == 1) and atbol(buf) end
 
+local function markbeforecur(buf)
+	return (buf.si < buf.ci) or (buf.si == buf.ci and buf.sj < buf.cj)
+end
+
+local function getcur(buf) return buf.ci, buf.cj end
+local function getsel(buf) return buf.si, buf.sj end
+
+local function getselbounds(buf)
+	if buf.si then
+		local ci, cj, si, sj = buf.ci, buf.cj, buf.si, buf.sj
+		local bi, bj, ei, ej
+		if si < ci then bi=si; bj=sj; ei=ci; ej=cj
+		elseif si > ci then bi=ci; bj=cj; ei=si; ej=sj
+		elseif sj < cj then bi=si; bj=sj; ei=ci; ej=cj
+		else -- sj >= cj
+			bi=ci; bj=cj; ei=si; ej=sj
+		end
+		return bi, bj, ei, ej
+	end
+end
+
+		
+
 -- modification at cursor line
 
-local function getline(buf)
+local function getline(buf, i)
 	-- return current line and cursor position in line
+	-- if i is provided, return line i
+	if i then return buf.ll[i], 1 end
 	return buf.ll[buf.ci], buf.cj
 end
 
@@ -295,6 +325,11 @@ end
 
 local function remline(buf)
 	table.remove(buf.ll, buf.ci)
+	buf.chgd = true
+end
+
+local function remnextline(buf)
+	table.remove(buf.ll, buf.ci+1)
 	buf.chgd = true
 end
 
@@ -373,7 +408,7 @@ end
 local function ainsch(k)
 	local l, cj = getline(buf)
 	setline(buf, l:sub(1, cj) .. char(k) .. l:sub(cj+1))
-	right()
+	aright()
 end
 
 local function aopenfile()
@@ -416,6 +451,43 @@ local function exch_mark()
 		buf.sj, buf.cj = buf.cj, buf.sj
 	end
 end
+
+local function copysel()
+	if not buf.si then msg("No selection."); return end
+	editor.kll = {}
+	local bi, bj, ei, ej = getselbounds(buf)
+	if bi == ei then -- all selection is within the line
+		local l = getline(buf)
+		editor.kll[1] = l:sub(bj, ej-1) -- if ei=bj, return nothing
+		return
+	end
+	-- si is not on the same line
+	editor.kll[1] = l:sub(bj)
+	for i = bi+1, ei-1 do
+		table.insert(editor.kll, getline(buf, i))
+	end
+	table.insert(editor.kll, getline(buf, ei):sub(1, ej-1)) -- last sel line
+	buf.si = nil
+end--copysel
+
+local function wipe()
+	if not buf.si then msg("No selection."); return end
+--~ 	copysel()
+	-- now delete sel
+	-- make sure cursor is at beg of selection
+	if markbeforecur(buf) then exch_mark() end 
+	local ci, cj = getcur(buf)
+	local si, sj = getsel(buf)
+	local l1, l2 = getline(buf), getline(buf, si)
+	setline(buf, l1:sub(1, cj) .. l2:sub(sj+1))
+	for i = ci+1, si do 
+		remnextline(buf) 
+	end
+	buf.si = nil
+	buf.chgd = true
+end--wipe
+
+
 ------------------------------------------------------------------------
 -- bindings
 
@@ -435,6 +507,7 @@ editor.edit_actions = { -- actions binding for text edition
 	[16] = aup,    -- ^P
 	[17] = function() editor.quit = true end, -- ^Q
 	[20] = atest,  -- ^T
+	[23] = wipe,   -- ^W
 	[24] = actrlx,  -- ^X
 	--
 	[keys.kpgup] = apgup,
@@ -480,7 +553,7 @@ function editor_loop()
 		else
 			msg(term.keyname(k) .. " not bound")
 		end
-	bufredisplay(buf)
+	redisplay(buf)
 	end--while true
 end
 
@@ -490,19 +563,17 @@ function main()
 	if not prevmode then print(prevmode, e, m); os.exit() end
 	term.setrawmode()
 	term.reset()
-	-- run the application
+	-- run the application in a protected call so we can properly reset
+	-- the tty mode and display a traceback in case of error
 	local ok, msg = xpcall(editor_loop, debug.traceback)
---~ 	local ok, msg = xpcall(edit, debug.debug, [edit args...])
-
 	-- restore terminal in a a clean state
 	term.show() -- show cursor
 	term.left(999); term.down(999)
 	style.normal()
-	io.flush()
+	flush()
 	--~ 	ln.setmode(omode)
 	term.restoremode(prevmode)
---~ 	print(prevmode)
-	if not ok then 
+	if not ok then -- display traceback in case of error
 		print(msg)
 		os.exit(1)
 	end
