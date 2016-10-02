4dd9c6e4513d93699320cf8f45e8ca40bae67da8
philanc <philanc@users.noreply.github.com>
2016-10-02 17:20:38 -0400

    merged term into editfile; clean up

--- junk/editfile.lua
+++ junk/editfile.lua
@@ -1,32 +1,21 @@
 -- editfile.lua
 -- !!! WORK IN PROGRESS !!!
--- @@@@ cursor and display bug when wipe/yank many lines (more than fit in scr)
+-- 
 ------------------------------------------------------------------------
-
---~ local he = require"he"
---~ he.interactive()
---~ local ln = require"linenoise"
-local term = require "term"
-
--- some local definitions and utility functions
+-- some local definitions (used by the module term and/or by the editor
 
 local strf = string.format
 local byte, char, rep = string.byte, string.char, string.rep
 local app, concat = table.insert, table.concat
+local yield = coroutine.yield
 
-local function repr(x) return strf("%q", tostring(x)) end
+local repr = function(x) return strf("%q", tostring(x)) end
 local function max(x, y) if x < y then return y else return x end end 
 local function min(x, y) if x < y then return x else return y end end 
 
 local function pad(s, w) -- pad a string to fit width w
 	if #s >= w then return s:sub(1,w) else return s .. rep(' ', w-#s) end
 end
-
-local out, outf, outdbg = term.out, term.outf, term.outdbg
-local go, cleareol, color = term.golc, term.cleareol, term.color
-local col, keys = term.colors, term.keys
-
-local flush = io.flush
 
 local function readfile(fn)
 	-- read file with filename 'fn' as a list of lines
@@ -37,11 +26,365 @@ local function readfile(fn)
 	fh:close()
 	return ll
 end
-	
+
+------------------------------------------------------------------------
+-- TERM MODULE
+
+--[[
+
+terminal utility module - unix only
+
+This module assumes the tty is in raw mode. 
+It provides functions based on stty (so available on unix) 
+to save, set and restore tty modes.
+
+-- a good ref on ANSI esc sequences:   
+https://en.wikipedia.org/wiki/ANSI_escape_code
+(in the text, "CSI" is "<esc>[")
+
+]]
 
 
 ------------------------------------------------------------------------
---display functions  (styles, boxes, line display)
+
+local out = io.write
+
+local function outf(...) 
+	-- write arguments to stdout, then flush.
+	io.write(...); io.flush()
+end
+
+local function outdbg(x, sep) 
+	out(repr(x):sub(2, -2))
+	if sep then out(sep) end
+	io.flush() 
+end
+
+-- following definitions (from term.clear to term.restore) are
+-- based on public domain code by Luiz Henrique de Figueiredo 
+-- http://lua-users.org/lists/lua-l/2009-12/msg00942.html
+term={
+	out = out,
+	outf = outf,
+	outdbg = outdbg,
+	clear = function() out("\027[2J") end,
+	cleareol = function() out("\027[K") end,
+	golc = function(l,c) out("\027[",l,";",c,"H") end,
+	up = function(n) out("\027[",n or 1,";","A") end,
+	down = function(n) out("\027[",n or 1,";","B") end,
+	right = function(n) out("\027[",n or 1,";","C") end,
+	left = function(n) out("\027[",n or 1,";","D") end,
+	color = function(f,b,m) 
+	    if m then out("\027[",f,";",b,";",m,"m")
+	    elseif b then out("\027[",f,";",b,"m")
+	    else out("\027[",f,"m") end 
+	end,
+	-- hide / show cursor
+	hide = function() out("\027[?25l") end,
+	show = function() out("\027[?25h") end,
+	-- save/restore cursor position
+	save = function() out("\027[s") end,
+	restore = function() out("\027[u") end,
+	-- reset terminal (clear and reset default colors)
+	reset = function() out("\027c") end,
+}
+
+term.colors = {
+	default = 0,
+	-- foreground colors
+	black = 30, red = 31, green = 32, yellow = 33, 
+	blue = 34, magenta = 35, cyan = 36, white = 37,
+	-- backgroud colors
+	bgblack = 40, bgred = 41, bggreen = 42, bgyellow = 43,
+	bgblue = 44, bgmagenta = 45, bgcyan = 46, bgwhite = 47,
+	-- attributes
+	reset = 0, normal= 0, bright= 1, bold = 1, reverse = 7,
+}
+
+------------------------------------------------------------------------
+-- key input
+
+term.keys = { -- key code definitions
+	unknown = 0x10000,
+	esc = 0x1b,
+	del = 0x7f,
+	kf1 = 0xffff,  -- 0xffff-0
+	kf2 = 0xfffe,  -- 0xffff-1
+	kf3 = 0xfffd,  -- ...
+	kf4 = 0xfffc,
+	kf5 = 0xfffb,
+	kf6 = 0xfffa,
+	kf7 = 0xfff9,
+	kf8 = 0xfff8,
+	kf9 = 0xfff7,
+	kf10 = 0xfff6,
+	kf11 = 0xfff5,
+	kf12 = 0xfff4,
+	kins  = 0xfff3,
+	kdel  = 0xfff2,
+	khome = 0xfff1,
+	kend  = 0xfff0,
+	kpgup = 0xffef,
+	kpgdn = 0xffee,
+	kup   = 0xffed,
+	kdown = 0xffec,
+	kleft = 0xffeb,
+	kright = 0xffea,
+}
+
+local keys = term.keys
+
+--special chars (for parsing esc sequences)
+local ESC, LETO, LBR, TIL= 27, 79, 91, 126  --  esc, [, ~
+
+local isdigitsc = function(c) 
+	-- return true if c is the code of a digit or ';'
+	return (c >= 48 and c < 58) or c == 59
+end
+
+--ansi sequence lookup table
+local seq = {
+	['[A'] = keys.kup,
+	['[B'] = keys.kdown,
+	['[C'] = keys.kright,
+	['[D'] = keys.kleft,
+
+	['[2~'] = keys.kins,
+	['[3~'] = keys.kdel,
+	['[5~'] = keys.kpgup,
+	['[6~'] = keys.kpgdn,
+	['[7~'] = keys.khome,  --rxvt
+	['[8~'] = keys.kend,   --rxvt
+	['[1~'] = keys.khome,  --linux
+	['[4~'] = keys.kend,   --linux
+	['[11~'] = keys.kf1,
+	['[12~'] = keys.kf2,
+	['[13~'] = keys.kf3,
+	['[14~'] = keys.kf4,
+	['[15~'] = keys.kf5,
+	['[17~'] = keys.kf6,
+	['[18~'] = keys.kf7,
+	['[19~'] = keys.kf8,
+	['[20~'] = keys.kf9,
+	['[21~'] = keys.kf10,
+	['[23~'] = keys.kf11,
+	['[24~'] = keys.kf12,
+
+	['OP'] = keys.kf1,   --xterm
+	['OQ'] = keys.kf2,   --xterm
+	['OR'] = keys.kf3,   --xterm
+	['OS'] = keys.kf4,   --xterm
+	['[H'] = keys.khome, --xterm
+	['[F'] = keys.kend,  --xterm
+
+	['[[A'] = keys.kf1,  --linux
+	['[[B'] = keys.kf2,  --linux
+	['[[C'] = keys.kf3,  --linux
+	['[[D'] = keys.kf4,  --linux
+	['[[E'] = keys.kf5,  --linux
+
+}
+
+local getcode = function() return byte(io.read(1)) end
+
+term.input = function()
+	-- return a "read next key" function that can be used in a loop
+	-- the "next" function blocks until a key is read
+	-- it returns ascii code for all regular keys, or a key code
+	-- for special keys (see term.keys)
+	-- (this function assume the tty is already in raw mode)
+	return coroutine.wrap(function()
+		local c, c1, c2, c3, c4, ci, s
+		while true do
+			c = getcode()
+			if c ~= ESC then -- not a seq, yield c
+				yield(c) 
+				goto continue
+			end 
+			c1 = getcode()
+			if c1 == ESC then -- esc esc [ ... sequence
+				yield(ESC)
+				-- here c still contains ESC, read a new c1
+				c1 = getcode() -- and carry on ...
+			end
+			if c1 ~= LBR and c1 ~= LETO then -- not a valid seq
+				yield(c) ; yield(c1)
+				goto continue
+			end
+			c2 = getcode()
+			s = char(c1, c2)
+			if c2 == LBR then -- esc[[x sequences (F1-F5 in linux console)
+				s = s .. char(getcode())
+			end
+			if seq[s] then 
+				yield(seq[s])
+				goto continue
+			end
+			if not isdigitsc(c2) then
+				yield(c) ; yield(c1) ; yield(c2)
+				goto continue
+			end
+			while true do
+				ci = getcode()
+				s = s .. char(ci)
+				if ci == TIL then 
+					if seq[s] then
+						yield(seq[s])
+						goto continue
+					else
+						-- valid but unknown sequence - ignore it
+						yield(keys.unknown)
+						goto continue
+					end
+				end
+				if not isdigitsc(ci) then
+					-- not a valid seq. return all the chars
+					yield(ESC)
+					for i = 1, #s do yield(byte(s, i)) end
+					goto continue
+				end
+			end--while
+			-- assume c is a regular char, return its ascii code
+			::continue::
+		end
+	end)--coroutine
+end--input()
+
+term.rawinput = function()
+	-- return a "read next key" function that can be used in a loop
+	-- the "next" function blocks until a key is read
+	-- it returns ascii code for all keys
+	-- (this function assume the tty is already in raw mode)
+	return coroutine.wrap(function()
+		local c
+		while true do
+			c = getcode()
+			yield(c) 
+		end
+	end)--coroutine
+end--rawinput()
+
+term.getcurpos = function()
+	-- return current cursor position (line, coloumn as integers)
+	--
+	outf("\027[6n") -- report cursor position. answer: esc[n;mR
+	local c, i = 0, 0
+	local s = ""
+	c = getcode(); if c ~= ESC then return nil end
+	c = getcode(); if c ~= LBR then return nil end
+	while true do
+		i = i + 1
+		if i > 8 then return nil end
+		c = getcode()
+		if c == byte'R' then break end
+		s = s .. char(c)
+	end
+	-- here s should be n;m
+	local n, m = s:match("(%d+);(%d+)")
+	if not n then return nil end
+	return tonumber(n), tonumber(m)
+end
+
+term.getscrlc = function()
+	-- return current screen dimensions (line, coloumn as integers)
+	term.save()
+	term.down(999); term.right(999)
+	local l, c = term.getcurpos()
+	term.restore()
+	return l, c
+end
+
+term.keyname = function(c)
+	for k, v in pairs(keys) do 
+		if c == v then return k end
+	end
+	if c < 32 then return "^" .. char(c+64) end
+	if c < 256 then return char(c) end
+	return tostring(c)
+end
+
+------------------------------------------------------------------------
+-- poor man's tty mode management, based on stty
+-- (better use slua linenoise extension if available)
+
+term.setrawmode = function()
+	return os.execute("stty raw -echo 2> /dev/null")
+end
+
+term.setsanemode = function()
+	return os.execute("stty sane")
+end
+
+term.savemode = function()
+	local fh = io.popen("stty -g")
+	local mode = fh:read('a')
+	print(mode)
+	local succ, e, msg = fh:close()
+	return succ and mode or nil, e, msg
+end
+
+term.restoremode = function(mode)
+	return os.execute("stty " .. mode)
+end
+
+-- return term -- END OF TERM MODULE
+
+
+------------------------------------------------------------------------
+-- EDITOR
+
+
+-- some local definitions
+
+-- the following is defined at the beginning of module term, above.
+-- local out, outf, outdbg = term.out, term.outf, term.outdbg
+
+local go, cleareol, color = term.golc, term.cleareol, term.color
+local col, keys = term.colors, term.keys
+local flush = io.flush
+
+	
+------------------------------------------------------------------------
+-- global objects and constants
+
+local tabln = 4
+local EOL = char(187) -- >>, indicate more undisplayed chars in s
+local NDC = char(183) -- middledot, used for non-displayable latin1 chars
+local EOT = '~'  -- used to indicate that we are past the end of text
+
+-- editor is the global editor object
+local editor = {
+	quit = false, -- set to true to quit editor_loop()
+	nextk = term.input(), -- the "read next key" function
+	buflist = {},  -- list of buffers
+	bufindex = 0,  -- index of current buffer
+}
+
+-- buf is the current buffer
+-- this is the same object as editor.buflist[editor.bufindex]
+local buf = {}  
+
+local function bufnew(ll)
+	-- create and initialize a new buffer object
+	-- ll is a list of lines
+	local buf = { 
+		ll=ll,        -- list of text lines
+		ci=1, cj=0,   -- text cursor (line ci, offset cj)
+		li=1,         -- index in ll of the line at the top of the box
+		chgd=true,    -- true if buffer has changed since last display
+		curstack = { {1, 0} } -- cursor stack (for push,pop opns)
+		-- box: a rectangular region of the screen where the buffer 
+		--      is displayed (see boxnew() below)
+		--      the box is assigned to the buffer by a layout function.
+		--      for the moment, the layout is performed by the 
+		--      fullredisplay() function.
+	}
+	return buf
+end
+
+
+------------------------------------------------------------------------
+-- screen display functions  (styles, boxes, line display)
 
 local style = {
 	normal = function() color(col.normal) end, 
@@ -79,12 +422,6 @@ local function boxfill(b, ch, stylefn)
 end
 
 -- line display
-
-local tabln = 4
-local EOL = char(187) -- >>, indicate more undisplayed chars in s
-local NDC = char(183) -- middledot, used for non-displayable latin1 chars
-local EOT = '~'  -- used to indicate that we are past the end of text
-
 
 local function ccrepr(b, j)
 	-- return display representation of char with code b
@@ -125,25 +462,8 @@ local function boxline(b, bl, l, insel, jon, joff)
 	style.normal()
 end --boxline
 
-
-------------------------------------------------------------------------
--- editor is the global editor object
-local editor = {
-	quit = false, -- set to true to quit editor_loop()
-	nextk = term.input(), -- the "read next key" function
-	buflist = {},  -- list of buffers
-	bufindex = 1,  -- index of current buffer ( the buf global variable)
-}
-
-
 ------------------------------------------------------------------------
 -- dialog functions
-
-local function status(m)
-	m = pad(m, editor.scrc)
-	go(1, 1); cleareol(); style.status()
-	out(m); style.normal(); flush()
-end
 
 local function msg(m)
 	-- display a message m on last screen line
@@ -173,9 +493,12 @@ local function readstr(prompt)
 	end--while
 end --readstr
 
-------------------------------------------------------------------------
--- buf is the current buffer
-local buf = {}
+local function status(m)
+	-- display a status string on top screen line
+	m = pad(m, editor.scrc)
+	go(1, 1); cleareol(); style.status()
+	out(m); style.normal(); flush()
+end
 
 local function statusline()
 	local s = strf("cur=%d,%d ", buf.ci, buf.cj)
@@ -186,19 +509,14 @@ local function statusline()
 	return s
 end--statusline
 
-local function bufnew(ll)
-	-- ll is a list of lines
-	local buf = { 
-		ll=ll,  -- list of text lines
-		ci=1, cj=0,   -- text cursor (line ci, offset cj)
-		li=1,      -- index in ll of the line at the top of the box
-		chgd=true, -- true if buffer has changed since last display
-		curstack = { {1, 0} } -- cursor stack (for push,pop opns)
-	}
-	return buf
-end
+
+------------------------------------------------------------------------
+-- screen redisplay functions
 
 local function adjcursor(buf)
+	-- adjust the screen cursor so that it matches with 
+	-- the buffer cursor (buf.ci, buf.cj)
+	--
 	-- first, adjust buf.li
 	local bl = buf.box.l
 	if buf.ci < buf.li or buf.ci >= buf.li+bl then 
@@ -225,13 +543,14 @@ local function adjcursor(buf)
 			break
 		end
 	end
-	status(statusline())
+	status(statusline()) 
 	go(buf.box.x + cx - 1, buf.box.y + cy - 1); flush()
 end -- adjcursor
 
 
 local function displaylines(buf)
-	-- display lines starting at index buf.li in list of lines buf.ll
+	-- display buffer lines starting at index buf.li 
+	-- in list of lines buf.ll
 	local b, ll, li = buf.box, buf.ll, buf.li
 	local ci, cj, si, sj = buf.ci, buf.cj, buf.si, buf.sj
 	local bi, bj, ei, ej
@@ -261,6 +580,7 @@ local function displaylines(buf)
 end
 
 local function redisplay(buf)
+	-- adjust cursor and repaint buffer lines if needed
 	adjcursor(buf)
 	if buf.chgd or buf.si then
 		displaylines(buf)
@@ -272,8 +592,13 @@ end --redisplay
 
 
 local function fullredisplay()
+	-- complete repaint of the screen
+	-- performed initially, or when a new buffer is displayed, or
+	-- when requested by the user (^L command) because the 
+	-- screen is garbled or the screensize has changed (xterm, ...)
+	--
 	editor.scrl, editor.scrc = term.getscrlc()
-	-- [editor.scrbox is a bckgnd box with a pattern to
+	-- [TMP!! editor.scrbox is a bckgnd box with a pattern to
 	-- visually check that edition does not overflow buf box]
 	editor.scrbox = boxnew(1, 1, editor.scrl, editor.scrc)
 	boxfill(editor.scrbox, NDC, style.bckg)
@@ -283,7 +608,8 @@ local function fullredisplay()
 end --fullredisplay
 
 ------------------------------------------------------------------------
--- buffer utility functions
+-- BUFFER AND CURSOR MANIPULATION 
+
 -- use these functions instead of direct buf.ll manipulation. 
 -- This will make it easier to change or enrich the 
 -- representation later. (eg. syntax coloring, undo/redo, ...)
@@ -684,13 +1010,9 @@ editor.esc_actions = {
 	[62] = e.goeot,  -- esc >
 }--esc_actions
 
-
-function editor_loop()
+function editor_loop(ll, fname)
 	style.normal()
-
-	tl = readfile'zztest' -- [TMP!! load default testfile]
-	e.newbuffer(tl, 'zztest'); 
-
+	e.newbuffer(ll, fname); 
 	while not editor.quit do
 		local k = editor.nextk()
 --~ 		if k == 17 then break end -- ^Q quits
@@ -710,6 +1032,13 @@ function editor_loop()
 end
 
 function main()
+	-- process argument
+	local ll, fname 
+	if arg[1] then
+		ll, err = readfile(arg[1]) -- load file as a list of lines
+		if not ll then print(err); os.exit(1) end
+		fname = arg[1]
+	end
 	-- set term in raw mode
 	local prevmode, e, m = term.savemode()
 	if not prevmode then print(prevmode, e, m); os.exit() end
@@ -717,7 +1046,7 @@ function main()
 	term.reset()
 	-- run the application in a protected call so we can properly reset
 	-- the tty mode and display a traceback in case of error
-	local ok, msg = xpcall(editor_loop, debug.traceback)
+	local ok, msg = xpcall(editor_loop, debug.traceback, ll, fname)
 	-- restore terminal in a a clean state
 	term.show() -- show cursor
 	term.left(999); term.down(999)
