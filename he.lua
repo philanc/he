7ba9790efcd88ac7d213cee7e22742ad4b0b9c05
philanc <philanc@users.noreply.github.com>
2019-06-21 19:10:14 -0400

    removed cmd, cmdlines; added shlines

--- he.lua
+++ he.lua
@@ -725,48 +725,24 @@ function he.shell(cmd)
 	return s, (exit=='signal' and status+128 or status)
 end
 
-function he.cmd(cmd)
-	-- wrap he.shell
-	-- redirect stderr to stdin
-	-- *** => cmd should not include redirections ***
-	-- return nil, stderr in case of error (status not 0)
-	cmd = cmd .. " 2>&1 " -- should work on unix and recent windows (win7?)
-	local r, m = he.shell(cmd)
-	if r and (m ~= 0) then 
-		m = r; r = nil 
-	elseif m == 0 then
-		m = nil
-	end
-	return r, m
-end --cmd()
-
-function he.cmdlines(cmd)
-	-- wrap os.popen
-	-- return command output as a list of lines
-	-- return nil, stderr, status in case of error (status not 0)
-	--
-	cmd = cmd .. " 2>&1 " -- should work on recent windows (xp,7?)
-	local f, m = io.popen(cmd) 
-	if not f then return nil, m end
-	local linelist = list()
-	local s
+function he.shlines(cmd)
+	-- same as shell() - returns a list of lines instead of a string
+	-- execute cmd; return stdout as a list of lines and a status code.
+	local f = io.popen(cmd) 
+	local linelist = list() 
 	while true do
-		s, m = f:read()
+		local s, m = f:read()
 		if not s then break end
 		linelist:insert(s)
-	end
+	end		
 	local succ, exit, status = f:close()
-	-- same status convention as he.shell():
-	-- if exit=='signal', return 128 + signal number
-	status = (exit=='signal' and status+128 or status)
-	if status ~= 0 then
-		m = linelist:concat('\n') -- in case of multi-line error msg
-		return nil, m, status
-	else
-		return linelist
-	end
-end --cmdlines()
-	
+	return linelist, (exit=='signal' and status+128 or status)
+end
+
+-- he.cmd removed
+-- to redirect stderr to stdout, append " 2>&1"
+-- should work on unix and recent windows - win7 also?)
+
 function he.escape_sh(s)  
 	-- escape posix shell special chars: [ ] ( ) ' " \
 	-- (works for unix, not windows...)
